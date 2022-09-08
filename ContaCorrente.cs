da733d58aea452804a6025b37d7d677db53cd084
JeanFusinatto <108768174+JeanFusinatto@users.noreply.github.com>
2022-09-08 19:06:17 -0300

    arquivos Program e ContaCorrente

---
+++ ContaCorrente.cs
@@ -0,0 +1,84 @@
+namespace POO.Banco{
+    public class ContaCorrente{
+        private string titular = "";
+        private string conta = "";
+        private int numeroAgencia;
+        private string nomeAgencia = "";
+        private double saldo;
+
+         public string GetTitular(){
+            return titular;
+        }
+
+        public void SetTitular(string titular){
+            this.titular = titular;
+        }
+
+        public string GetConta(){
+            return conta;
+        }
+        
+        public void SetConta(string conta){
+            this.conta = conta;
+        }
+
+        public int GetNumeroAgencia(){
+            return numeroAgencia;
+        }
+        
+        public void SetNumeroAgencia(int numeroAgencia){
+            this.numeroAgencia = numeroAgencia;
+        }
+        
+        public string GetNomeAgencia(){
+            return nomeAgencia;
+        }
+        public void SetNomeAgencia(string nomeAgencia){
+            this.nomeAgencia = nomeAgencia;
+        }
+
+        public double GetSaldo(){
+            return saldo;
+        }
+        public void SetSaldo(double saldo){
+            this.saldo = saldo;
+        }
+
+        public bool Depositar(double valor){
+            if(valor <= 0){
+                return false;
+            }
+
+            saldo = saldo + valor;
+            return true;
+        }
+
+        public bool Sacar(double valor){
+            if(valor <= 0 || saldo < valor){
+                return false;
+            }
+
+            saldo = saldo - valor;
+            return true;
+        }
+
+        public bool Transferir(double valor, ContaCorrente conta){
+            if(!Sacar(valor)){
+                return false;
+            }
+
+            conta.Depositar(valor);
+
+            return true;
+        }
+
+        public void DadosConta(){
+            Console.WriteLine("Titular: " + GetTitular());
+            Console.WriteLine("Conta: " + GetConta());
+            Console.WriteLine("Número da Agência: " + GetNumeroAgencia());
+            Console.WriteLine("Nome da Agência: " + GetNomeAgencia());
+            Console.WriteLine("Saldo Maria: " + GetSaldo());
+        } 
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using POO.Banco;
+using POO.Confeccao;
+
+namespace POO{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.SetTitular("Mariazinha");
+            conta1.SetConta("654321-7");
+            conta1.SetNumeroAgencia(123);
+            conta1.SetNomeAgencia("Agencia central DV");
+            conta1.SetSaldo(100);
+
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.SetTitular("Joaozinho");
+            conta2.SetConta("123456-7");
+            conta2.SetNumeroAgencia(123);
+            conta2.SetNomeAgencia("Agencia central DV");
+            conta2.SetSaldo(0);
+
+            conta1.Transferir(160, conta2);
+            
+            conta1.DadosConta();
+            conta2.DadosConta();
+        }
+    }
+}
